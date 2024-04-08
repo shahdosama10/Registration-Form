d9ae0fcbcb3d052aa2079739d9b8ee9e52d4f638
Seif <SE.SeifIbrahim@gmail.com>
2024-04-09 01:32:25 +0200

    got the ids

--- API_Ops.php
+++ API_Ops.php
@@ -1,1 +1,46 @@
-<!-- API logic -->
+<?php
+
+$birthdate = $_GET['birthdate'];
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://imdb8.p.rapidapi.com/actors/v2/get-born-today?today=$birthdate&first=20",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+        "X-RapidAPI-Host: imdb8.p.rapidapi.com",
+        "X-RapidAPI-Key: 4648805e9fmshdb928ab630e4755p16c96fjsn3872a1f3e683"
+    ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    $actor_names = get_actor_names($response);
+}
+
+function get_actor_names($response) {
+	$actor_ids = array();
+	$response = json_decode($response, true);
+	$nodes_list = $response['data']['bornToday']['edges'];
+	foreach ($nodes_list as $node) {
+		array_push($actor_ids, $node['node']['id']);
+	}
+	foreach ($actor_ids as $actor_id) {
+		echo "$actor_id <br>";
+	}
+
+	$actor_names = array();
+
+	return $actor_names;
+}
--- indexScripts.js
+++ indexScripts.js
@@ -1,93 +1,93 @@
-function validateForm() {
-    // Get form inputs
-    var fullName = document.getElementById('full_name').value;
-    var userName = document.getElementById('user_name').value;
-    var birthdate = document.getElementById('birthdate').value;
-    var phone = document.getElementById('phone').value;
-    var address = document.getElementById('address').value;
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('confirm_password').value;
-    var email = document.getElementById('email').value;
-    var userImage = document.getElementById('user_image').value;
+// function validateForm() {
+//     // Get form inputs
+//     var fullName = document.getElementById('full_name').value;
+//     var userName = document.getElementById('user_name').value;
+//     var birthdate = document.getElementById('birthdate').value;
+//     var phone = document.getElementById('phone').value;
+//     var address = document.getElementById('address').value;
+//     var password = document.getElementById('password').value;
+//     var confirmPassword = document.getElementById('confirm_password').value;
+//     var email = document.getElementById('email').value;
+//     var userImage = document.getElementById('user_image').value;
 
-    // Regular expression patterns
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    var passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*()\-_=+{};:,<.>ยง~])[a-zA-Z0-9!@#$%^&*()\-_=+{};:,<.>ยง~]{8,}$/;
-    var phonePattern = /^\+?([0-9]\s?){6,14}[0-9]$/;
-    var namePattern = /^[a-zA-Z\s]+$/;
+//     // Regular expression patterns
+//     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     var passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*()\-_=+{};:,<.>ยง~])[a-zA-Z0-9!@#$%^&*()\-_=+{};:,<.>ยง~]{8,}$/;
+//     var phonePattern = /^\+?([0-9]\s?){6,14}[0-9]$/;
+//     var namePattern = /^[a-zA-Z\s]+$/;
 
-    // Clear previous error messages
-    clearErrors();
+//     // Clear previous error messages
+//     clearErrors();
 
-    // Validate full name
-    if (fullName.trim() === '') {
-        showError('full_name', 'Please enter your full name.');
-        scrollToError('full_name');
-        return false;
-    } else if (!namePattern.test(fullName)) {
-        showError('full_name', 'Please enter a valid full name.');
-        scrollToError('full_name');
-        return false;
-    }
+//     // Validate full name
+//     if (fullName.trim() === '') {
+//         showError('full_name', 'Please enter your full name.');
+//         scrollToError('full_name');
+//         return false;
+//     } else if (!namePattern.test(fullName)) {
+//         showError('full_name', 'Please enter a valid full name.');
+//         scrollToError('full_name');
+//         return false;
+//     }
 
-    // Validate username
-    if (userName.trim() === '') {
-        showError('user_name', 'Please enter a username.');
-        scrollToError('user_name');
-        return false;
-    }
+//     // Validate username
+//     if (userName.trim() === '') {
+//         showError('user_name', 'Please enter a username.');
+//         scrollToError('user_name');
+//         return false;
+//     }
 
-    // Validate birthdate
-    if (birthdate === '') {
-        showError('birthdate', 'Please select your birthdate.');
-        scrollToError('birthdate');
-        return false;
-    }
+//     // Validate birthdate
+//     if (birthdate === '') {
+//         showError('birthdate', 'Please select your birthdate.');
+//         scrollToError('birthdate');
+//         return false;
+//     }
 
-    // Validate phone
-    if (phone.trim() === '' || !phonePattern.test(phone)) {
-        showError('phone', 'Please enter a valid phone number.');
-        scrollToError('phone');
-        return false;
-    }
+//     // Validate phone
+//     if (phone.trim() === '' || !phonePattern.test(phone)) {
+//         showError('phone', 'Please enter a valid phone number.');
+//         scrollToError('phone');
+//         return false;
+//     }
 
-    // Validate address
-    if (address.trim() === '') {
-        showError('address', 'Please enter your address.');
-        scrollToError('address');
-        return false;
-    }
+//     // Validate address
+//     if (address.trim() === '') {
+//         showError('address', 'Please enter your address.');
+//         scrollToError('address');
+//         return false;
+//     }
 
-    // Validate password
-    if (!passwordPattern.test(password)) {
-        showError('password', 'Password must be at least 8 characters long and contain at least one number and one special character.');
-        scrollToError('password');
-        return false;
-    }
+//     // Validate password
+//     if (!passwordPattern.test(password)) {
+//         showError('password', 'Password must be at least 8 characters long and contain at least one number and one special character.');
+//         scrollToError('password');
+//         return false;
+//     }
 
-    // Confirm password
-    if (password !== confirmPassword) {
-        showError('confirm_password', 'Passwords do not match.');
-        scrollToError('confirm_password');
-        return false;
-    }
+//     // Confirm password
+//     if (password !== confirmPassword) {
+//         showError('confirm_password', 'Passwords do not match.');
+//         scrollToError('confirm_password');
+//         return false;
+//     }
 
-    // Validate email
-    if (!emailPattern.test(email)) {
-        showError('email', 'Please enter a valid email address.');
-        scrollToError('email');
-        return false;
-    }
+//     // Validate email
+//     if (!emailPattern.test(email)) {
+//         showError('email', 'Please enter a valid email address.');
+//         scrollToError('email');
+//         return false;
+//     }
 
-    // Validate user image
-    if (userImage.trim() === '') {
-        showError('user_image', 'Please upload your profile picture.');
-        scrollToError('user_image');
-        return false;
-    }
+//     // Validate user image
+//     if (userImage.trim() === '') {
+//         showError('user_image', 'Please upload your profile picture.');
+//         scrollToError('user_image');
+//         return false;
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
 // Function to show error message text
 function showError(inputId, message) {
@@ -111,4 +111,27 @@ function clearErrors() {
 function scrollToError(inputId) {
     var inputField = document.getElementById(inputId);
     inputField.scrollIntoView({ behavior: 'smooth', block: 'center' });
+}
+
+
+function checkActors() {
+    var birthdate = document.getElementById("birthdate").value;
+    var birthdateParts = birthdate.split("-");
+    var month = birthdateParts[1];
+    var day = birthdateParts[2];
+    var formattedDate = month + "-" + day;
+    
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            if (xhr.status == 200) {
+                document.getElementById("actorsResult").innerHTML = xhr.responseText;
+            } else {
+                console.error("Error:", xhr.statusText);
+            }
+        }
+    };
+    
+    xhr.open("GET", "API_Ops.php?birthdate=" + formattedDate, true);
+    xhr.send();
 }
